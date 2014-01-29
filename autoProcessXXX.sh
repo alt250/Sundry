627576aa4a20bccd45db789e30c0456e212fee8a
overbyrn <richmsubs@gmail.com>
2014-01-29 10:29:39 +0000

    added .jpg to extensions to be deleted added -f flag to mv command to ensure files moved to destination do not fail due to existing target file renamed variable "dirname" to ensure it does not interfere with the call out to "dirname" when getting parentdir added logic to the move section to check the file sizes. if a larger (720p) mp4 file exists at the target location already, then assume we want to keep the newer smaller non-720p mp4 just downloaded.  Therefore force an overwrite during move.  Otherwise take no action. changed clean-up files find command. -regexp to -iregexp for case-insensitive. regex syntax change to extensions

--- autoProcessXXX.sh
+++ autoProcessXXX.sh
@@ -30,8 +30,15 @@
 #          - check for scene name convention.  eg. .XXX. and strip everything from XXX to end of filename
 #          - move processed file to final location
 # 22/07/13 - added .txt to extensions to be deleted
+# 18/01/14 - added .jpg to extensions to be deleted
+#          - added -f flag to mv command to ensure files moved to destination do not fail due to existing target file
+#          - renamed variable "dirname" to ensure it does not interfere with the call out to "dirname" when getting parentdir
+#          - added logic to the move section to check the file sizes. if a larger (720p) mp4 file exists at the target location already, then
+#          - assume we want to keep the newer smaller non-720p mp4 just downloaded.  Therefore force an overwrite during move.  Otherwise take
+#          - no action.  Hopefully this will see fewer .mp4 files lurking in the Unknown directory, needing manual tidying.
+# 29/01/14 - changed clean-up files find command. -regexp to -iregexp for caseinsensitive. regex syntax change to extentions 
 
-DESTDIR=/mnt/user/Stuff2
+DESTDIR=/mnt/eddie/Stuff2
 
 if [ ! -z $7 ] && [ $7 -gt 0 ]; then
     echo "post-processing failed, bypassing script"
@@ -49,13 +56,15 @@ echo "Removing supplimentary files for $1/$3"
 # .url
 # .html
 # .txt/TXT
+# .jpg
 
 # example:
 # find "/mnt/user/NG/Unknown" -iname "TopGrl.2011.03.07.Lil.Niki.Nymph.XXX.720p.MP4-hUSHhUSH" -regextype posix-awk -regex "(.*\.nfo|.*\.diz)" -exec echo rm -f {} +
 
 # look for files with extensions as per regex, delete them if found
 echo -n "Cleaning extra support files... "
-find "$1" -iname "*.*" -regextype posix-awk -regex "(.*\.nfo|.*\.diz|.*\.url|.*\.html|.*\.txt|.*\.TXT)" -exec rm -f {} +
+find "$1" -regextype posix-extended -iregex '.*\.(nfo|diz|url|htm|html|jpg|png|txt)' -exec rm -f {} +
+
 echo -e "Done\n"
 
 # rename extracted file to name of parent directory and move to parent directory
@@ -63,16 +72,17 @@ echo "Renaming extracted file to name of parent directory... "
 while read file
 do
   parentdir="$(dirname "$1")"
-  dirname="$(dirname "$file")"
-  new_name="${dirname##*/}"
+  directoryname="$(dirname "$file")"
+  new_name="${directoryname##*/}"
   file_ext=${file##*.}
   echo "file: $file"
   echo "new_name: $new_name"
   echo "file_ext: $file_ext"  
   echo "parentdir: $parentdir"
-  echo "dirname: $dirname"
-  if [ -n "$file_ext" -a  -n "$dirname" -a -n "$new_name" ]
+  echo "directoryname: $directoryname"
+  if [ -n "$file_ext" -a  -n "$directoryname" -a -n "$new_name" ]
   then
+    echo "mv $file $parentdir/$new_name.$file_ext"
     mv "$file" "$parentdir/$new_name.$file_ext"
   fi
 done < <(find "$1" -maxdepth 1 -type f)
@@ -83,7 +93,10 @@ echo -n "Checking for Scene Naming Convention... "
 if [[ "$new_name" == *.XXX.* ]]; then
   echo "Found"
   oldscenename="$new_name"
+  echo "Old Name : $new_name"
   new_name="${oldscenename%.XXX.*}"
+  echo "New Name : $new_name"
+  echo "mv $parentdir/$oldscenename.$file_ext $parentdir/$new_name.$file_ext"
   mv "$parentdir/$oldscenename.$file_ext" "$parentdir/$new_name.$file_ext"
 else
   echo "Not found"
@@ -91,14 +104,29 @@ fi
 echo -e "Done\n"
 
 # move processed file to dest location
-echo -n "Moving processed file to $DESTDIR... "
-mv "$parentdir/$new_name.$file_ext" $DESTDIR
+echo "Moving processed file to $DESTDIR... "
+src_file_size=$(stat -c%s "$parentdir/$new_name.$file_ext")
+#echo "Source File Size = $src_file_size"
+if [ -f $DESTDIR/$new_name.$file_ext ]; then # a file already exists at the target location
+  echo "Found file with same name at target location.  Checking file size..."
+  tgt_file_size=$(stat -c%s "$DESTDIR"/"$new_name"."$file_ext")
+  #echo "Target File Size = $src_file_size"
+  if [ "$src_file_size" -lt "$tgt_file_size" ]; then # source file is smaller than target
+    echo "Source file is smaller than target, I'm going to go ahead and assume its safe to overwrite as we want to keep the smaller non-720p version"
+    echo "mv -f $parentdir/$new_name.$file_ext $DESTDIR"
+    mv -f "$parentdir/$new_name.$file_ext" "$DESTDIR"
+  fi
+else # no file found at target location, go ahead and move it
+  echo "mv $parentdir/$new_name.$file_ext $DESTDIR"
+  mv "$parentdir/$new_name.$file_ext" "$DESTDIR"
+fi
 echo -e "Done\n"
 
 # check if old subdirectory now empty and remove if so
 echo -n "Deleting old sub-directory..."
 if [ -z "`find $1 -type f`" ]; then
   echo "Empty, deleting"
+  echo "rmdir $1"
   rmdir "$1"
 else
   echo "Directory not empty!"
@@ -107,7 +135,7 @@ echo -e "Done\n"
 
 # call remove from history child process. spawns child process with no-hangup
 echo "Calling child script to delete entry from SABnzbd history"
-CMDLINE="/bin/bash /mnt/cache/Services/sabnzbd/scripts/delfromhist.sh $2 >/dev/null 2>&1"
+CMDLINE="/bin/bash /opt/sabnzbd/scripts/delfromhist.sh $2 >/dev/null 2>&1"
 echo "CMDLINE: $CMDLINE"
 nohup $CMDLINE > /dev/null 2>&1 &
 echo "Exiting script '$(basename \"${0}\")'"
